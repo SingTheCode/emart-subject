708499fbcc66980173c62c51d852f07a48a74a45
spiderq2697 <spiderq10@gmail.com>
2022-08-27 12:41:34 +0900

    refactor: 테스트 컴포넌트의 이름을 변경한다

--- client/src/stories/Content.stories.tsx
+++ client/src/stories/Content.stories.tsx
@@ -15,7 +15,14 @@ const Template: ComponentStory<typeof Content> = (args) => (
   <Content {...args} />
 );
 
-export const Default = Template.bind({});
-Default.args = {
+export const Event = Template.bind({});
+Event.args = {
   content: "신세계포인트 적립 시 25% 할인",
+  type: "event",
+};
+
+export const Discount = Template.bind({});
+Discount.args = {
+  content: "신세계포인트 적립 시 25% 할인",
+  type: "discount",
 };
