c1d645b0631c2cddd85c1a18e8fe98e1ef2fcc6d
spiderq2697 <spiderq10@gmail.com>
2022-08-30 01:53:23 +0900

    feat: 메뉴바의 scroll바를 숨긴다

--- client/src/components/blocks/TopMenuBar/styles.ts
+++ client/src/components/blocks/TopMenuBar/styles.ts
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 const MenuBar = styled.div`
+  border-bottom: 1px solid #f3f3f3;
   overflow: scroll;
   overflow: auto;
   white-space: nowrap;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export default {
