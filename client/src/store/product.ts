0be74fc058e7bf4b8f63722474343d1d01c0808a
spiderq2697 <spiderq10@gmail.com>
2022-08-30 12:37:48 +0900

    feat: json-server를 페이지네이션 api로 변경한다
    - "pagination/"으로 요청을 보내면 pagination에 대한 정보와 contents를 응답한다

--- client/src/store/product.ts
+++ client/src/store/product.ts
@@ -1,22 +1,48 @@
-import { selector } from "recoil";
+import { atom, selector } from "recoil";
 import $ from "jquery";
 
 import { selectedMenuState } from "./menu";
+import { PaginationResponse } from "utils/interfaces";
 
-export const productListSelector = selector({
+const productPageState = atom({
+  key: "ProductPageState",
+  default: 1,
+});
+
+export const productListSelector = selector<PaginationResponse>({
   key: "ProductListSelector",
   get: async ({ get }) => {
     try {
       const response = await $.ajax({
-        type: "get",
-        url: `http://localhost:3001/productList/?type=${get(
+        url: `http://localhost:3001/pagination/?type=${get(
           selectedMenuState
-        )}`,
+        )}&pageNumber=${get(productPageState)}`,
+        type: "get",
+        contentType: "application/x-www-form-urlencoded; charset=euc-kr",
         dataType: "json",
+        success: (result) => console.log(result),
       });
-      return response;
+      return response[0];
     } catch (err) {
       console.log(err);
     }
   },
 });
+
+// export const productListSelector = selector({
+//   key: "ProductListSelector",
+//   get: async ({ get }) => {
+//     try {
+//       const response = await $.ajax({
+//         type: "get",
+//         url: `http://localhost:3001/productList/?type=${get(
+//           selectedMenuState
+//         )}`,
+//         dataType: "json",
+//       });
+//       return response;
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   },
+// });
